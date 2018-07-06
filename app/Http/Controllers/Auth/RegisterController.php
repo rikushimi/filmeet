eb2df22914ac5763c763f3dbf54abe83e07bb3f1
masayukimihira <ft.lvlv.dcn@gamil.com>
2018-07-06 04:10:07 +0000

    masa

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
     use RegistersUsers;
 
 
-    protected $redirectTo = '/';
+    protected $redirectTo = '/show';
 
     /**
      * Create a new controller instance.
