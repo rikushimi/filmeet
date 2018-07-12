50bc1c784df6252e0d579c22630310a2e3a1093b
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-07-12 07:57:35 +0000

    codeblue

---
+++ resources/views/movies/time.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+
+@section('content')
+ 
+  <!--The Greatest Showman-->
+ 
+<div class = "movie">
+{{$title}}<br> <img src="https://image.tmdb.org/t/p/w500{{$image}}">
+</div>
+
+<div class="panel-body">
+  
+  <p>見たいよボタン押してる人一覧</p>
+ 
+   <div class=want_users>
+     @foreach ($want_users as $user)
+      <ul>
+        <li>{{ $user->name }}</li>
+      </ul>
+     @endforeach
+   </div>
+</div>
+@endsection
