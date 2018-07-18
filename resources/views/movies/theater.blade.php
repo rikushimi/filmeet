548c378968f5f6aeaf725954926f6a67f5ca856d
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-07-18 05:48:30 +0000

    erinacho

--- resources/views/movies/theater.blade.php
+++ resources/views/movies/theater.blade.php
@@ -5,29 +5,19 @@
 
 
  <div class = "eiga-title">
-     <p class="eiga-title-mein">{{$title}}</p><br>
-     <p class="eiga-title-tag">{{$tag}}</p>
+     <p1>{{$title}}</p1><br>
+     <p2>{{$tag}}</p2>
  </div>
  
  <div class = "eiga-image">
  <img src="https://image.tmdb.org/t/p/w500{{$poster}}">
  </div>
 
-<<<<<<< HEAD
 <div class = "eiga-kan"> 
  <p class ="eiga-overview"> 
     {{$overview}}<br>
     {{$release}}
  </p>
-=======
- <div class ="eiga-kan"> 
- <p class="eiga-overview">{{$overview}}<br>
- {{$release}}</p>
- </div>
- 
-
- @include('button.want_button',['code'=>$code])
->>>>>>> 4cc3535c1f18d0b6acd1995ebde656a517a772a6
  
 @if(Auth::user()->is_wanting($code))
   
