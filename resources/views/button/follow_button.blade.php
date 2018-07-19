2bb1e3424682e7c69ada0ba7cb300aef26a65989
kazane.fujimoto <kazane.fujimoto@rakuten.com>
2018-07-19 01:15:03 +0000

    201807171014

--- resources/views/button/follow_button.blade.php
+++ resources/views/button/follow_button.blade.php
@@ -3,13 +3,13 @@
         {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
             {!! Form::hidden('code',$code) !!}
             {!! Form::hidden('id',$user->id)!!}
-            {!! Form::submit('てかやっぱり見たくない', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('てかやっぱり見たくない', ['id' => "followbutton"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
             {!! Form::hidden('code',$code) !!}
             {!! Form::hidden('id',$user->id)!!}
-            {!! Form::submit('共に見ましょう', ['class' => "btn btn-primary btn-block"]) !!}
+            {!! Form::submit('共に見ましょう', ['id' => "followbutton"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
--- resources/views/movies/theater.blade.php
+++ resources/views/movies/theater.blade.php
@@ -1,36 +1,44 @@
- <body id="eiga-ichiran">
+<body id="eiga-ichiran">
 @extends('layouts.app')
 
 @section('content')
 
-
  <div class = "eiga-title">
-     <p1>{{$title}}</p1><br>
-     <p2>{{$tag}}</p2>
+     <p class="eiga-title-mein">{{$title}}</p><br>
+     <p class="eiga-title-tag">{{$tag}}</p>
  </div>
  
- <div class = "eiga-image">
- <img src="https://image.tmdb.org/t/p/w500{{$poster}}">
+ 
+ <div id ="left">
+         <div class = "eiga-image">
+         <img src="https://image.tmdb.org/t/p/w500{{$poster}}">
+         </div>
  </div>
 
-<div class = "eiga-kan"> 
- <p class ="eiga-overview"> 
-    {{$overview}}<br>
-    {{$release}}
- </p>
- 
-@if(Auth::user()->is_wanting($code))
-  
-  <div class ="want_users">
-    @foreach ($want_users as $user)
-      <ul>
-        <li><a href="{{ url('profile'). $user->id}}">{{$user->name}}</a></li>
-      </ul>
-      @include('button.follow_button',['code'=>$code,'user' => $user])
-    @endforeach
-  </div>
-  
-@endif 
+ <div id="right">
+        <div class = "eiga-kan"> 
+                <p class ="eiga-overview"> 
+                    {{$overview}}<br>
+                    {{$release}}
+                </p>
+        
+                @if(Auth::user()->is_wanting($code))
+        
+                <div class ="watchlist"> 
+                  <p class="watchlist-p1">List of people who wanna watch it</p>
+                </div>
+                
+                <div class ="want_users">
+                    @foreach ($want_users as $user)
+                      <ul>
+                        <li><a href="{{ url('profile'). $user->id}}">{{$user->name}}</a></li>
+                        <li>@include('button.follow_button',['code'=>$code,'user' => $user])</li>
+                      </ul>
+                    @endforeach
+                </div>
+          
+                @endif 
+        </div>
  </div>
  
   @include('button.want_button',['code'=>$code])
