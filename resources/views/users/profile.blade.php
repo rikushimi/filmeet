9e0cf572fb2ede7872abd105e4197a6a7ff60f8d
kazane.fujimoto <kazane.fujimoto@rakuten.com>
2018-07-20 00:49:47 +0000

    07200920

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -15,30 +15,29 @@
  <div class="right">
           
      <div class="menu">
-           <li><a href="{{route('profile.get',  ['id' => $user->id]) }}">Profile</a></li>
+          <li><a href="{{route('profile.get',  ['id' => $user->id]) }}">Profile</a></li>
           @if (Auth::id() == $user->id)
-              <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Like<span class="badge">{{ $count_followings }}</span></a></li>
+          <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Like<span class="badge">{{ $count_followings }}</span></a></li>
           @endif
           @if (Auth::id() == $user->id)
-              <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Liked<span class="badge">{{ $count_followers }}</span></a></li>
+          <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Liked<span class="badge">{{ $count_followers }}</span></a></li>
           @endif
-           <li role="presentation" class="{{ Request::is('users/*/mymovies') ? 'active' : '' }}"><a href="{{ route('mymovies.get', ['id' => $user->id]) }}">My Movies<span class="badge"></a></li>
-
-          <li><a href="#">chat</a></li><div class="comment">
-        </div>
+          <li role="presentation" class="{{ Request::is('users/*/mymovies') ? 'active' : '' }}"><a href="{{ route('mymovies.get', ['id' => $user->id]) }}">My Movies<span class="badge"></a></li>
+          <li><a href="#">chat</a></li>
+     </div>
+          
+    <div class="comment">
         <p class="comment1">Favorite Movies </p>
         <p class="comment1-text">the greatest showman</p><br><br><br>
         <p class="comment2">Comment </p>
         <p class="comment2-text">I like movie very much! Shall we go to theater?</p>
-          
+    </div>
+         
         @if (Auth::id() == $user->id)
-            <div id="edit_button">
+            <div class="edit_button">
               <a href="{{route('profile.edit', Auth::id())}}">edit</a>
             </div>
         @endif
-        
-       
-
  </div>
 
 
