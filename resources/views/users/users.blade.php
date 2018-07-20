88d115ca3c95487ab64cd732edfa360ad4fc9089
erina.katagiri <erina.katagiri@rakuten.com>
2018-07-20 03:59:44 +0000

    followfollowerDONE

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,13 +1,12 @@
-
-
 @if (count($users) > 0)
 <ul class="media-list">
-   
+
     
-@foreach ($users as $user)
+@foreach ($friends as $user)
     <li class="media">
         <div class="media-body">
             <div>
+                <li><img src="https://image.tmdb.org/t/p/w500{{$user->image}}">{{$user->moviename}}</li>
                 {{ $user->name}}
                {!! link_to_route('profile.get', 'View profile', ['id' => $user->id]) !!}
             </div>
