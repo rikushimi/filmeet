6412f004f829875ed0e021a532d878fac34ab617
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-07-24 07:25:31 +0000

    sss

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -100,10 +100,12 @@ class ProfileController extends Controller
         $followings = $user->followings()->paginate(10);
         
         $followlist=\DB::table('user_follow')
-        ->select('users.name','users.id', 'movies.name as moviename', 'movies.image', 'movies.code') 
-        ->join('users', 'users.id', '=' , 'user_follow.follow_id')
-        ->join('movies', 'movies.code', '=' , 'user_follow.code')
-        ->where('user_follow.user_id', $user->id)->get();
+        ->select('id') 
+        // ->select('users.name','users.id', 'movies.name as moviename', 'movies.image', 'movies.code') 
+        // ->join('users', 'users.id', '=' , 'user_follow.follow_id')
+        // ->join('movies', 'movies.code', '=' , 'user_follow.code')
+        // ->where('user_follow.user_id', $user->id)->get();
+        ->get();
   
        $url = \DB::table('users')->where('id',$id)->select('image_url')->first();
        $url = $url->image_url;
