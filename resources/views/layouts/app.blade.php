9d3451db33957fa0b314ebd565c7d48a1f6b36ab
kazane.fujimoto <kazane.fujimoto@rakuten.com>
2018-07-24 00:24:08 +0000

    happybirthdaywatashi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,25 +16,26 @@
          
 
          <!-- google font -->
-         <link href="https://fonts.googleapis.com/css?family=Alegreya+SC|Josefin+Slab|Playfair+Display+SC|Allura|Arimo|Cinzel|Cookie|Crimson+Text|Gaegu|Julius+Sans+One|Lobster|Open+Sans|Source+Serif+Pro|Quicksand|Satisfy" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Alegreya+SC|Hind|Josefin+Slab|Playfair+Display+SC|Allura|Arimo|Cinzel|Cookie|Crimson+Text|Gaegu|Julius+Sans+One|Lobster|Open+Sans|Source+Serif+Pro|Quicksand|Satisfy" rel="stylesheet">
          
          
          <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
          
          <link href="{{ secure_asset('css/slick.css') }}" rel="stylesheet">
+         <script src="src/cta.js"></script>
     </head>
-    <body>
-      
-           @include('commons.navbar')
-           
+        <body>
           
-        <div class="container">
-            @include('commons.error_messages')
-
-            @yield('content')
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="{{ secure_asset('js/slick.min.js') }}"></script>
-        <script src="{{ secure_asset('js/style.js') }}"></script>
-    </body>
+               @include('commons.navbar')
+               
+              
+            <div class="container">
+                @include('commons.error_messages')
+    
+                @yield('content')
+            </div>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <script src="{{ secure_asset('js/slick.min.js') }}"></script>
+            <script src="{{ secure_asset('js/style.js') }}"></script>
+        </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,30 +13,52 @@
         <li class="enjoyfilmeet">Lets</li>
         <li class="enjoyfilmeet">get</li>
         <li class="enjoyfilmeet">it</li>
-        <li class="enjoyfilmeet">started.</li>
+        <li class="enjoyfilmeet">started !</li>
         </ul>
 
         <p>
-            <a class="toshowbalde" href="{{url('show')}}"> <span class="glyphicon glyphicon-glyphicon glyphicon-heart"></span></a>
-            
-            
-            
-            <!--{!! link_to_route('show.get', 'start✔', null) !!}-->
+            <a class="toshowbalde" href="#gray" > <span class="glyphicon glyphicon-glyphicon glyphicon-heart"></span></a>
         </p>
+        
      </div>
      
-     <div class="explanation">
+    <!-- <div class="explanation">-->
+    <!--     <div class="explanation1">-->
+    <!--        <p> welcome to filmeet</p> -->
+    <!--     </div>     -->
+    <!--         <div class="explanation2">-->
+    <!--             <p>-->
+    <!--                 Select movies you wanna watch and Find people you like.<br>-->
+    <!--                 Don't be shy!<br>-->
+    <!--                 Filmeets give you best matches!<br>-->
+    <!--                 First, fill out your profile to find your best movie buddies.<br>-->
+    <!--                 <a href="{{route('profile.get',Auth::user()->id)}}">Profile</a>-->
+    <!--             </p> -->
+    <!--        </div>    -->
+    <!--</div>   -->
+    <div id="gray">
+　　</div>
+　　<div id="green">
+　　    <div class="explanation">
          <div class="explanation1">
-            <p> welcome to filmeet</p> 
+            <p> Welcome to filmeet !</p> 
          </div>     
              <div class="explanation2">
                  <p>
-                 Select movies you wanna watch and Find people you like.<br>
-                 Don't be shy!<br>
-                 Filmeet gives you best matches!<br>
-                  </p> 
+                     Select movies you wanna watch and Find people you like.<br>
+                     Don't be shy!<br>
+                     Filmeets give you best matches!<br><br>
+                     First, fill out your profile to find your best movie buddies.<br>
+                     <a href="{{route('profile.get',Auth::user()->id)}}">- Profile -</a><br><br>
+                     Or you can see Movie List right now!<br>
+                     <a href="{{url('show')}}">- Now Playing Movies -</a>
+                 </p> 
             </div>    
-    </div>     
+    </div>   
+	<a href="#" class="abtn">
+		<div class="close"></div>
+	</a>
+    </div>
     @else
     <div class="center">
         <div class="text-center">
