c0f2508a93d6f56afe068866dd35f33baf72288f
erina.katagiri <erina.katagiri@rakuten.com>
2018-07-25 00:51:02 +0000

    masashimda

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -26,7 +26,7 @@
           @if (Auth::id() == $user->id)
           <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Sent Requests<span class="badge">{{ $count_followings }}</span></a></li>
           <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Recieved Requests<span class="badge">{{ $count_followers }}</span></a></li>
-          <li role="presentation" class="{{ Request::is('users/*/mymovies') ? 'active' : '' }}"><a href="{{ route('mymovies.get', ['id' => $user->id]) }}">My Movies/Matches&nbsp;<span class="glyphicon glyphicon-heart"></span></a></li>
+          <li role="presentation" class="{{ Request::is('users/*/mymovies') ? 'active' : '' }}"><a href="{{ route('mymovies.get', ['id' => $user->id]) }}">Match List&nbsp;<span class="glyphicon glyphicon-heart"></span></a></li>
           @endif
      </div>
 
@@ -38,7 +38,7 @@
         <p class="comment2-text">{!! nl2br(e( $user->comment )) !!}</p>
         
     </div>
-        <div class="center">
+        <div class="p-center">
         @if (Auth::id() == $user->id)
             <div class="edit_button">
               <a href="{{route('profile.edit',  ['id' => $user->id])}}">edit</a>
